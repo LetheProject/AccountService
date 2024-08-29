00c6038456c7deadb1a463bb5f8fafb89304f622
a-moseman <drewmoseman@gmail.com>
2024-08-29 10:13:23 -0400

    Add javadoc

--- src/main/java/org/amoseman/accountservice/pojo/AccountDetails.java
+++ src/main/java/org/amoseman/accountservice/pojo/AccountDetails.java
@@ -2,12 +2,22 @@ package org.amoseman.accountservice.pojo;
 
 import org.bson.Document;
 
+/**
+ * Encapsulates the details of an account.
+ */
 public class AccountDetails {
     private final String username;
     private final String displayName;
     private final String pronouns;
     private final String description;
 
+    /**
+     * Instantiate an account details object.
+     * @param username the username.
+     * @param displayName the display name.
+     * @param pronouns the pronouns.
+     * @param description the description.
+     */
     public AccountDetails(String username, String displayName, String pronouns, String description) {
         this.username = username;
         this.displayName = displayName;
@@ -15,6 +25,10 @@ public class AccountDetails {
         this.description = description;
     }
 
+    /**
+     * Instantiate an account details object.
+     * @param document the document to extract the username, display name, pronouns, and description from.
+     */
     public AccountDetails(Document document) {
         this.username = document.getString("username");
         this.displayName = document.getString("display-name");
